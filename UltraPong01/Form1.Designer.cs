82738c814e949ad33081780cbabbc067b2d33501
Doug Parr <temporalcoder@gmail.com>
2024-06-21 08:26:35 +0100

    Added paddles

--- UltraPong01/Form1.Designer.cs
+++ UltraPong01/Form1.Designer.cs
@@ -31,6 +31,9 @@
             this.components = new System.ComponentModel.Container();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -46,21 +49,51 @@
             // 
             // timer1
             // 
-            this.timer1.Enabled = true;
             this.timer1.Interval = 33;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(105, 432);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "StartSerial";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(105, 469);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(124, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "com5";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(513, 432);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(187, 59);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "com5";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 523);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -68,6 +101,9 @@
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- UltraPong01/Form1.cs
+++ UltraPong01/Form1.cs
@@ -7,15 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
+
 
 namespace UltraPong01
 {
     public partial class Form1 : Form
     {
+
+        string portNum = "COM5"; //edit
+        SerialPort sp;
+        bool serialOpen = false;
+
         int bx = 20;
         int by = 20;
         int bxv = 5;
         int byv = 5;
+
+        //Player 1
+        int p1x = 10;
+        int p1y = 200;
+        int p1h = 50; //height! 
+
+        //Player 2
+        int p2x = 400; //use PB Width!
+        int p2y = 200;
+        int p2h = 50; //height! 
 
         public Form1()
         {
@@ -30,6 +47,15 @@ namespace UltraPong01
             if (bx < 0 || bx > pictureBox1.Width-20) { bxv *= -1; }
             if (by < 0 || by > pictureBox1.Height-20) { byv *= -1; }
 
+            //update player positions
+
+
+            //get serial Data
+            if (serialOpen)
+            {
+                p1y = getSerialData();
+            }
+           
             Refresh(); //calls the paint function
         }
 
@@ -37,6 +63,47 @@ namespace UltraPong01
         {
             Pen whitePen = new Pen(Color.White, 2);
             e.Graphics.DrawEllipse(whitePen, bx, by, 20, 20);
+
+            e.Graphics.DrawRectangle(whitePen, p1x, p1y, 10, p1h);
+            e.Graphics.DrawRectangle(whitePen, p2x, p2y, 10, p2h);
+
+        }
+
+        void startSerial()
+        {
+            portNum = textBox1.Text;
+            if (portNum.Contains("COM")) // 'some' validation
+            {
+                sp = new SerialPort(portNum, 9600, Parity.None, 8, StopBits.One);
+                sp.Open();
+                sp.ReadTimeout = 10000;
+                textBox2.Text = ("Connecting to " + portNum);
+
+                button1.BackColor = Color.GreenYellow;
+                timer1.Enabled = true;
+                serialOpen = true;
+            }
+        }
+
+        int getSerialData()
+        {
+            if (sp.IsOpen)
+            {
+                string buffer = sp.ReadExisting();
+                if (buffer != null)
+                {
+                    if (buffer.Contains("Button1Pressed"))
+                    {
+                        textBox2.Text = buffer; //for debugging
+                    }
+                }
+            }          
+            return 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            startSerial();
         }
     }
 }
